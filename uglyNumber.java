0a1d95c914650f2269675955aa4d2ddb225a9e7e
harsh <sinha.harshraj1101@gmail.com>
2024-08-21 22:26:40 +0530

    Ugly number

---
+++ uglyNumber.java
@@ -0,0 +1,16 @@
+public class uglyNumber{
+    public static boolean isUgly(int n) {
+        if(n <= 0)
+            return false;
+        while(n % 2 == 0) n /= 2;
+        while(n % 3 == 0) n /= 3;
+        while(n % 5 == 0) n /= 5;
+
+        return (n == 1);
+    }
+    public static void main(String[] args){
+        int n = 6;
+        boolean res = isUgly(n);
+        System.out.println(res);
+    }
+}
