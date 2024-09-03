3a9b86ffbfff95ae5c387f72d3753df8e3ef7fb9
harsh <sinha.harshraj1101@gmail.com>
2024-09-03 22:45:57 +0530

    Deleted one line

--- MaximumPointsYouCanObtainfromCards.java
+++ MaximumPointsYouCanObtainfromCards.java
@@ -1,7 +1,6 @@
 public class MaximumPointsYouCanObtainfromCards{
     public static int maxScore(int[] cardPoints, int k) {
         int lsum =0, rsum =0, max =0,sum =0;
-        int n = cardPoints.length;
         for(int i=0;i<k;i++){
             lsum = lsum + cardPoints[i];
         }
