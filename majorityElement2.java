858adf52903097565e3b81a9aff0bef48f915fc2
harsh <sinha.harshraj1101@gmail.com>
2024-09-05 20:33:57 +0530

    added better approach of solving

--- majorityElement2.java
+++ majorityElement2.java
@@ -1,25 +1,44 @@
 import java.util.*;
 public class majorityElement2 {
+    // public static List<Integer> majorityElement(int[] nums) {
+    //     Arrays.sort(nums);
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     for(int num : nums){
+    //         map.put(num, map.getOrDefault(num, 0) + 1);
+    //     }
+    //     int freqOfElement = nums.length/3;
+    //     List<Integer> al = new ArrayList<>();
+    //     for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+    //         int freq = entry.getValue();
+    //         int key = entry.getKey();
+    //         if(freq > freqOfElement){
+    //             al.add(key);
+    //         }
+    //         else{
+    //             continue;
+    //         }
+    //     }
+    //     return al;
+    // }
+
+    // with better approach
     public static List<Integer> majorityElement(int[] nums) {
-        Arrays.sort(nums);
         Map<Integer, Integer> map = new HashMap<>();
         for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
-        int freqOfElement = nums.length/3;
+
         List<Integer> al = new ArrayList<>();
+        int limit = nums.length/3;
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            int freq = entry.getValue();
-            int key = entry.getKey();
-            if(freq > freqOfElement){
-                al.add(key);
-            }
-            else{
-                continue;
-            }
+            int ele = entry.getKey();
+            int val = entry.getValue();
+            if(val > limit)
+                al.add(ele);
         }
         return al;
     }
+
     public static void main(String[] args) {
         int[] arr = {3,2,3};
         System.out.println(majorityElement(arr));
