3fa67f38005b1dcd2ef9450113a1d2b8323684ac
harsh <sinha.harshraj1101@gmail.com>
2024-09-13 03:11:07 +0530

    1684. Count the Number of Consistent Strings

---
+++ consistentString.java
@@ -0,0 +1,24 @@
+public class consistentString {
+    public static int countConsistentStrings(String allowed, String[] words) {
+        var map = new boolean[26];
+        var res = words.length;
+
+        for (var c : allowed.toCharArray())
+        map[c - 'a'] = true;
+
+        for (var word : words) {
+        for (var c : word.toCharArray()) {
+            if (!map[c - 'a']) {
+            res--;
+            break;
+            }
+        }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        String allowed = "ab";
+        String [] words = {"ad","bd","aaab","baa","badab"};
+        System.out.println(countConsistentStrings(allowed,words));
+    }
+}
