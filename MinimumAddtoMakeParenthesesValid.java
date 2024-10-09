597e3397616b3216b63da8bed148b8c52a6044cc
harsh <sinha.harshraj1101@gmail.com>
2024-10-09 20:24:03 +0530

    921. Minimum Add to Make Parentheses Valid

--- MinimumAddtoMakeParenthesesValid.java
+++ MinimumAddtoMakeParenthesesValid.java
@@ -1,21 +1,37 @@
-import java.util.Stack;
+// import java.util.Stack;
 public class MinimumAddtoMakeParenthesesValid {
+    // public static int minAddToMakeValid(String s) {
+    //     Stack<Character> st = new Stack<>();
+    //     for(int i = 0 ; i < s.length() ; i++){
+    //         char ch = s.charAt(i);
+    //         if(ch == '(')
+    //             st.push(ch);
+    //         else{
+    //             if(st.isEmpty() || st.peek() == ')'){
+    //                 st.push(ch);
+    //             }
+    //             else{
+    //                 st.pop();
+    //             }
+    //         }
+    //     }
+    //     return st.size();
+    // }
+
     public static int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
+        int open = 0, close = 0;
+        char[] arr = s.toCharArray();
+        for(int ch : arr){
             if(ch == '(')
-                st.push(ch);
+                open++;
             else{
-                if(st.isEmpty() || st.peek() == ')'){
-                    st.push(ch);
-                }
-                else{
-                    st.pop();
-                }
+                if(open <= 0)
+                    close++;
+                else
+                    open--;
             }
         }
-        return st.size();
+        return open + close;
     }
     public static void main(String[] args){
         String s = "())";
