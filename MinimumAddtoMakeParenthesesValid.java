115ef7eb34e3b7ad6e6dd15f3a7f1adf284d1ec5
harsh <sinha.harshraj1101@gmail.com>
2024-10-09 20:14:25 +0530

    921. Minimum Add to Make Parentheses Valid

---
+++ MinimumAddtoMakeParenthesesValid.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+public class MinimumAddtoMakeParenthesesValid {
+    public static int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch == '(')
+                st.push(ch);
+            else{
+                if(st.isEmpty() || st.peek() == ')'){
+                    st.push(ch);
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return st.size();
+    }
+    public static void main(String[] args){
+        String s = "())";
+        System.out.println(minAddToMakeValid(s));
+    }
+}
