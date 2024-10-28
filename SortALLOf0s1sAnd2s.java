ba9dd1494569f2a72c071e96b1683b32b8d6ec3b
harsh <sinha.harshraj1101@gmail.com>
2024-10-28 19:42:36 +0530

    Sort a linked list of 0s, 1s and 2s (optimal solution)

--- SortALLOf0s1sAnd2s.java
+++ SortALLOf0s1sAnd2s.java
@@ -8,31 +8,32 @@ class Node {
 }
 public class SortALLOf0s1sAnd2s {
     public static Node segregate(Node head) {
+        if(head == null || head.next == null) return head;
+        Node zeroHead = new Node(-1);
+        Node oneHead = new Node(-1);
+        Node twoHead = new Node(-1);
+        Node zero = zeroHead, one = oneHead, two = twoHead;
         Node temp = head;
-        int count0 = 0, count1 = 0, count2 = 0;
         while(temp != null){
-            if(temp.data == 0) count0++;
-            else if(temp.data == 1) count1++;
-            else count2++;
-            temp = temp.next;
-        }
-        temp = head;
-        while(temp != null){
-            if(count0 > 0) {
-                temp.data = 0;
-                count0--;
+            if(temp.data == 0){
+                zero.next = temp;
+                zero = temp;
             }
-            else if(count1 > 0){
-                temp.data = 1;
-                count1--;
+            else if(temp.data == 1){
+                one.next = temp;
+                one = temp;
             }
             else{
-                temp.data = 2;
-                count2--;
+                two.next = temp;
+                two = temp;
             }
             temp = temp.next;
         }
-        return head;
+        zero.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+        Node newHead = zeroHead.next;
+        return newHead;
     }
 
     public static void printList(Node head) {
