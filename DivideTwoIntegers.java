cd2e50da5693d11769d45f17014540ca567a4532
harsh <sinha.harshraj1101@gmail.com>
2024-11-03 21:37:14 +0530

    used better approach

--- DivideTwoIntegers.java
+++ DivideTwoIntegers.java
@@ -1,12 +1,19 @@
 public class DivideTwoIntegers {
     public static int divide(int dividend, int divisor) {
-        if(dividend == Integer.MIN_VALUE && divisor == -1){
-            return Integer.MAX_VALUE;
+        int n = 0;
+        long c = 0, sign = 1;
+        if(divisor == 1) return dividend;
+        if((dividend < 0 && divisor >= 0) || (dividend >= 0 && divisor < 0))
+            sign = -1;
+        long dd = Math.abs((long) dividend);
+        long dr = Math.abs((long) divisor);
+        for(int i = 30 ; i >= 0 ; i--){
+            if(dd >= (dr << i)){
+                c += (1 << i);
+                dd -= (dr << i);
+            }
         }
-        else{
-            int c = dividend/divisor;
-            return c;
-        }
+        return (int) (c * sign);
     }
     public static void main(String[] args) {
         int dividend = 10;
