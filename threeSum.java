39a8b88f4bf3a9191e617d6d8cf3b242f1ce66b8
harsh <sinha.harshraj1101@gmail.com>
2024-11-10 23:55:53 +0530

    15. 3Sum(Brute force)

--- threeSum.java
+++ threeSum.java
@@ -1,37 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class threeSum {
     public static List<List<Integer>> sum(int[] nums) {
-        int target = 0;
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length - 2 ; i++){
-            if(i > 0 && nums[i] == nums[i - 1]){
-                continue;
-            }
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == target){
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while(j < k && nums[j] == nums[j + 1]){
-                        j++;
+        Set<List<Integer>> st = new HashSet<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = i + 1 ; j < nums.length ; j++){
+                for(int k = j + 1 ; k < nums.length ; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                        temp.sort(null);
+                        st.add(temp);
                     }
-                    while(j < k && nums[k] == nums[k - 1]){
-                        k--;
-                    }
-                    j++;
-                    k--;
-                }else if(sum < target){
-                    j++;
-                }else{
-                    k--;
                 }
             }
         }
+        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
     }
     public static void main(String[] args) {
