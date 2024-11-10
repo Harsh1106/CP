63c026b800e771234204fc55c19130afb905dc7e
harsh <sinha.harshraj1101@gmail.com>
2024-11-11 00:21:40 +0530

    15. 3Sum(Optimal Approach)

--- threeSum.java
+++ threeSum.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 public class threeSum {
     public static List<List<Integer>> sum(int[] nums) {
-        // Better Approach
-        Set<List<Integer>> st = new HashSet<>();
+        // Optimal Approach
+        Arrays.sort(nums);
+        List<List<Integer>> al = new ArrayList<>();
         for(int i = 0 ; i < nums.length ; i++){
-            HashSet<Integer> temp = new HashSet<>();
-            for(int j = i + 1 ; j < nums.length ; j++){
-                int third = -(nums[i] + nums[j]);
-                if(temp.contains(third)){
-                    List<Integer> al = Arrays.asList(nums[i], nums[j], third);
-                    al.sort(null);
-                    st.add(al);
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0) j++;
+                else if(sum > 0) k--;
+                else{
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    al.add(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
                 }
-                temp.add(nums[j]);
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
-        return ans;
+        return al;
     }
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
